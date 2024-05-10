1f04240644331ed21ae07ab65c82498715b5da90
huy.vu <huy.vu@hsc.com.vn>
2024-05-10 17:56:57 +0700

    history_page_and_validate

--- src/App.js
+++ src/App.js
@@ -7,19 +7,25 @@ import Badminton from './page/Badminton';
 import Football from './page/Football';
 import ContactUs from './page/ContactUs';
 import Register from './page/register/Register';
+import Historyyy from './page/history/Historyyy';
+import { useState } from 'react';
 
-function App() {
+function App() { 
+    const [Infor,setInfor]= useState([])
     // const [count, setCount] = useState(2);
     return (
+       
         <div className='App'>
             <Routes>
                 <Route element={<HomePage />} path='/' />
-                <Route element={<Register />} path='/register' />
+                <Route element={<Register Infor={Infor} setInfor={setInfor}/>} path='/register'/>
+                <Route element={<Historyyy Infor={Infor} setInfor={setInfor}/>} path='/history'/>
                 <Route element={<BasketBall />} path='/basketball' />
                 <Route element={<BaseBall />} path='/baseball' />
                 <Route element={<Badminton />} path='/badminton' />
                 <Route element={<Football />} path='/football' />
                 <Route element={<ContactUs />} path='/contact' />
+                
             </Routes>
         </div>
     );
