25e057c5e7a3fff199d8bf4fe6e80475301ed6d3
Ngo Trung Hieu <ngohieu138@gmail.com>
2024-05-13 18:47:29 +0700

    add booking

--- src/App.js
+++ src/App.js
@@ -7,31 +7,30 @@ import Football from './page/Football';
 import ContactUs from './page/ContactUs';
 import Register from './page/register/Register';
 import Historyyy from './page/history/Historyyy';
-import Sports from './page/Sports'
-import Tennis from './page/Tennis'
-import BookStadium from './page/BookStadium'
+import Sports from './page/Sports';
+import Tennis from './page/Tennis';
+import BookStadium from './page/BookStadium';
 import { useState } from 'react';
+import BookingHistory from './page/BookingHistory';
 
-function App() { 
-    const [Infor,setInfor]= useState([])
+function App() {
+    const [Infor, setInfor] = useState([]);
     // const [count, setCount] = useState(2);
     return (
-       
         <div className='App'>
             <Routes>
                 <Route element={<HomePage />} path='/' />
-                <Route element={<Register Infor={Infor} setInfor={setInfor}/>} path='/register'/>
-                <Route element={<Historyyy Infor={Infor} setInfor={setInfor}/>} path='/history'/>
-                <Route element={<Sports />} path='/sports'>
-                <Route element={<BookStadium />} path='bookstadium' />
-                </Route>
+                <Route element={<Register Infor={Infor} setInfor={setInfor} />} path='/register' />
+                <Route element={<Historyyy Infor={Infor} setInfor={setInfor} />} path='/history' />
+                <Route element={<Sports />} path='/sports' />
+                <Route element={<BookStadium />} path='/bookstadium' />
+                <Route element={<BookingHistory />} path='/bookingHistory' />
                 <Route element={<Register />} path='/register' />
                 <Route element={<BasketBall />} path='/basketball' />
                 <Route element={<BaseBall />} path='/baseball' />
                 <Route element={<Tennis />} path='/tennis' />
                 <Route element={<Football />} path='/football' />
                 <Route element={<ContactUs />} path='/contact' />
-                
             </Routes>
         </div>
     );
---
+++ src/components/BookingStadium/BookingCalendar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function BookingCalendar(props) {
+    return <div></div>;
+}
+
+export default BookingCalendar;
