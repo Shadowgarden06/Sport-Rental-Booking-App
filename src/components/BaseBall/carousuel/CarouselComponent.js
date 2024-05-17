dca93ca4c0b688b94f41fc3a5e8e732e779fc9e5
Ngo Trung Hieu <ngohieu138@gmail.com>
2024-05-18 00:22:28 +0700

    fix_UI

--- src/components/BaseBall/carousuel/CarouselComponent.js
+++ src/components/BaseBall/carousuel/CarouselComponent.js
@@ -6,7 +6,7 @@ function CarouselComponent(props) {
     return (
         <div className='parallax-content7 position-relative'>
             <div className='text-center text-light p-4 mt-5'>
-                <span className='fs-3 text-uppercase text-white'>Ryan BaseBall</span>
+                <span className='fs-3 text-uppercase text-white mt-5'>Ryan BaseBall</span>
                 <h1 className='fs-78 lh-1 my-2'>
                     <span>
                         <span className='text-gray text-uppercase fw-bold'>Attitude</span> is{' '}
